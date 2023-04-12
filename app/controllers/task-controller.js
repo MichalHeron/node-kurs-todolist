1f4a132446d6db5f294298fe0f0b8e70e034ff7c
Heron <michklak91@gmail.com>
2023-04-12 21:44:40 +0200

    first commit, added show, delete, toggle done with connection with db

---
+++ app/controllers/task-controller.js
@@ -0,0 +1,95 @@
+const Task = require('../db/models/task');
+
+class TaskController {
+
+  async showTasks(req, res) {
+    // pobierz wszystkie taski i wyświetl na widoku
+    const tasks = await Task.find({})
+    res.render('pages/tasks/index', {tasks});
+  }
+
+  showCreateForm(req, res) {
+    // wyświetl formularz nowego taska
+    res.render('pages/tasks/create');
+  }
+
+  async create(req, res) {
+    // przygotuj nowy task
+    const task = new Task({
+      title: req.body.name,
+      description: req.body.description,
+
+    })
+    try {
+      await task.save()
+      res.redirect('/' );
+    } catch (e) {
+      // jeśli są błędy, wyświetl ja na widoku
+      console.log("blad")
+      console.log(req.body)
+      res.render('pages/tasks/create', {
+				errors: e.errors,
+				form: req.body,
+			});
+    }
+  }
+
+  async showEditForm(req, res) {
+    // pobierz task i wyświetl formularz edycji
+
+    // mongoose posiada metodę .findById
+    // Przykła: task = await Task.findById('60fec8c43501d21b309befbd')
+    // https://mongoosejs.com/docs/api.html#model_Model.findById
+
+    res.render('pages/tasks/edit');
+  }
+
+  async edit(req, res) {
+    // pobierz task
+
+    // zaktualizuj dane
+
+    try {
+      // zapisz i przekieruj na stronę główną
+      res.redirect('/');
+    } catch (e) {
+      // jeśli są błędy, wyświetl ja na widoku
+      res.render('pages/tasks/edit');
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params
+    try {
+      // usuń task i przekieruj na stronę główną
+      await Task.deleteOne({_id: id})
+      res.redirect('/');
+    } catch (e) {
+      console.log("blad podczas usuwania")
+      // opcjonalnie obsłuż błąd
+    }
+  }
+
+  async toggleDone(req, res) {
+    // pobierz task
+    // zmień wartość "done" taska (na odwrotną, czyli z 1 na 0, lub z 0 na 1) 
+    // oraz przekieruj na stronę główną
+    const { id } = req.params
+    const task = await Task.findOne({_id : id})
+    task.done = task.done ? 0 : 1;
+    task.title = task.title
+    task.description = task.description
+    console.log(task)
+
+    try {
+       await task.save()
+       res.redirect('/')
+        }catch (e)
+          {
+           console.log("error")
+          }
+  }
+  
+}
+
+module.exports = new TaskController();
